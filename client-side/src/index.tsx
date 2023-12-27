1c7ee55eecda5689e2ba5b765fd4afd77551ab10
Kacper Dudek <kacper.dudek98@gmail.com>
2023-12-27 14:25:39 +0100

    Styles added

--- client-side/src/index.tsx
+++ client-side/src/index.tsx
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import 'semantic-ui-css/semantic.min.css'
-import './app/layout/styles.css'
-import { StoreContext, store } from './app/stores/store.ts'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router/Routes.tsx'
+import ReactDOM from 'react-dom/client';
+import 'react-calendar/dist/Calendar.css'; 
+import 'semantic-ui-css/semantic.min.css';
+import { StoreContext, store } from './app/stores/store.ts';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes.tsx';
+import './app/layout/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <StoreContext.Provider value={store}> 
